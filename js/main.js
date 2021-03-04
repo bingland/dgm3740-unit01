37e5fbf8c33589752de8676fa39ece94faf64c23
Brayden England <bray.patriot@gmail.com>
2021-03-03 22:16:37 -0700

    Reactive nav bar items + mobile nav bar and js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+let openMenu = document.querySelector('.open')
+let hiddenMenu = document.querySelector('.hiddenNav')
 
-
+openMenu.addEventListener('click', () => {
+    if (hiddenMenu.style.display !== "block") {
+        hiddenMenu.style.display = "block"
+    } else {
+        hiddenMenu.style.display = "none"
+    }
+})
